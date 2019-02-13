01d7d3ecd4f539ef1404438353ff53dcf26ab052
blackwell <blackwellsmithiv@gmail.com>
2019-02-13 14:57:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
 class Transfer
-  # your code here
+  attr_accessor :amount, :sender, :receiver, :status
+  def initialize(sender,receiver,amount)
+    @amount = amount
+    @receiver = receiver
+    @sender = sender
+    @status = "pending"
+  end
+  def valid?
+    if @sender.valid? == true && @receiver.valid? == true && @sender.balance >= amount 
+      true 
+    else 
+      false 
+    end
+  end
+  def execute_transaction
+    
+  end 
 end
